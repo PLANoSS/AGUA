8ce7bf149725bd384cc2ae347078ad05d5afb499
Fabián <freyesbueno@gmail.com>
2018-10-29 09:22:16 -0500

    cambios

--- layers/Conocimiento sobre ley de aguas.js
+++ layers/Conocimiento sobre ley de aguas.js
@@ -22,12 +22,12 @@ jsonSource_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconex
                 declutter: true,
                 source:jsonSource_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0, 
                 style: style_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0,
-    title: 'Pagar el uso de agua dependiendo de la cantidad que utilice cada familia pero con excepciones<br />\
+    title: '<b/>Percepción sobre el pago del servicio de agua:</b><br />El degradado de colores en el mapa muestra el porcentaje de personas<br />que creen que el servicio debe ser pagado dependiendo la cantidad que<br />use cada familia, pero con excepciones para evitar problemas sociales.<br />\
     <img src="styles/legend/Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0_0.png" />  0 - 20 <br />\
     <img src="styles/legend/Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0_1.png" />  20 - 40 <br />\
     <img src="styles/legend/Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0_2.png" />  40 - 60 <br />\
     <img src="styles/legend/Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0_3.png" />  60 - 80 <br />\
-    <img src="styles/legend/Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0_4.png" />  80 - 100 <br />'
+    <img src="styles/legend/Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0_4.png" />  80 - 100 <br /><br />'
         });var format_Elconsumodeaguaseaunserviciopblico_1 = new ol.format.GeoJSON();
 var features_Elconsumodeaguaseaunserviciopblico_1 = format_Elconsumodeaguaseaunserviciopblico_1.readFeatures(json_Elconsumodeaguaseaunserviciopblico_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -38,12 +38,12 @@ jsonSource_Elconsumodeaguaseaunserviciopblico_1.addFeatures(features_Elconsumode
                 declutter: true,
                 source:jsonSource_Elconsumodeaguaseaunserviciopblico_1, 
                 style: style_Elconsumodeaguaseaunserviciopblico_1,
-    title: 'El consumo de agua sea un servicio público<br />\
+    title: '<b/>Percepción sobre el servicio de agua para consumo humano y riego:</b><br />El degradado de colores en el mapa muestra el porcentaje de personas<br />que prefieren que el servicio sea público, no privado.<br />\
     <img src="styles/legend/Elconsumodeaguaseaunserviciopblico_1_0.png" />  0 - 20 <br />\
     <img src="styles/legend/Elconsumodeaguaseaunserviciopblico_1_1.png" />  20 - 40 <br />\
     <img src="styles/legend/Elconsumodeaguaseaunserviciopblico_1_2.png" />  40 - 60 <br />\
     <img src="styles/legend/Elconsumodeaguaseaunserviciopblico_1_3.png" />  60 - 80 <br />\
-    <img src="styles/legend/Elconsumodeaguaseaunserviciopblico_1_4.png" />  80 - 100 <br />'
+    <img src="styles/legend/Elconsumodeaguaseaunserviciopblico_1_4.png" />  80 - 100 <br /><br />'
         });var format_NotieneconocimientosobrelaLeydeAguas_2 = new ol.format.GeoJSON();
 var features_NotieneconocimientosobrelaLeydeAguas_2 = format_NotieneconocimientosobrelaLeydeAguas_2.readFeatures(json_NotieneconocimientosobrelaLeydeAguas_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -54,12 +54,12 @@ jsonSource_NotieneconocimientosobrelaLeydeAguas_2.addFeatures(features_Notieneco
                 declutter: true,
                 source:jsonSource_NotieneconocimientosobrelaLeydeAguas_2, 
                 style: style_NotieneconocimientosobrelaLeydeAguas_2,
-    title: 'No tiene conocimiento sobre la Ley de Aguas<br />\
+    title: '<b/>Conocimiento sobre la Ley de aguas:</b><br />El degradado de colores en el mapa muestra el porcentaje de personas<br />que no tienen conocimiento sobre la Ley de aguas.<br />\
     <img src="styles/legend/NotieneconocimientosobrelaLeydeAguas_2_0.png" />  0 - 20 <br />\
     <img src="styles/legend/NotieneconocimientosobrelaLeydeAguas_2_1.png" />  20 - 40 <br />\
     <img src="styles/legend/NotieneconocimientosobrelaLeydeAguas_2_2.png" />  40 - 60 <br />\
     <img src="styles/legend/NotieneconocimientosobrelaLeydeAguas_2_3.png" />  60 - 80 <br />\
-    <img src="styles/legend/NotieneconocimientosobrelaLeydeAguas_2_4.png" />  80 - 100 <br />'
+    <img src="styles/legend/NotieneconocimientosobrelaLeydeAguas_2_4.png" />  80 - 100 <br /><br />'
         });var format_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3 = new ol.format.GeoJSON();
 var features_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3 = format_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3.readFeatures(json_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -70,12 +70,12 @@ jsonSource_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3.addFeatures
                 declutter: true,
                 source:jsonSource_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3, 
                 style: style_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3,
-    title: 'Reducir los problemas de agua siendo eficientes con su uso en casa<br />\
+    title: '<b/>Percepción sobre reducir los problemas de agua:</b><br />El degradado de colores en el mapa muestra el porcentaje de personas<br />que creen que ser eficientes con el uso de agua en casa, reduciría<br />problemas de agua.<br />\
     <img src="styles/legend/Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3_0.png" />  0 - 20 <br />\
     <img src="styles/legend/Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3_1.png" />  20 - 40 <br />\
     <img src="styles/legend/Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3_2.png" />  40 - 60 <br />\
     <img src="styles/legend/Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3_3.png" />  60 - 80 <br />\
-    <img src="styles/legend/Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3_4.png" />  80 - 100 <br />'
+    <img src="styles/legend/Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3_4.png" />  80 - 100 <br /><br />'
         });var format_Calidaddeaguaensucantnsehamantenidoigual_4 = new ol.format.GeoJSON();
 var features_Calidaddeaguaensucantnsehamantenidoigual_4 = format_Calidaddeaguaensucantnsehamantenidoigual_4.readFeatures(json_Calidaddeaguaensucantnsehamantenidoigual_4, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
@@ -86,15 +86,15 @@ jsonSource_Calidaddeaguaensucantnsehamantenidoigual_4.addFeatures(features_Calid
                 declutter: true,
                 source:jsonSource_Calidaddeaguaensucantnsehamantenidoigual_4, 
                 style: style_Calidaddeaguaensucantnsehamantenidoigual_4,
-    title: 'Calidad de agua en su cantón se ha mantenido igual<br />\
+    title: '<b/>Percepción sobre la calidad del agua:</b><br />El degradado de colores en el mapa muestra el porcentaje de personas<br />que creen la calidad de agua en su cantón se ha mantenido igual.<br />\
     <img src="styles/legend/Calidaddeaguaensucantnsehamantenidoigual_4_0.png" />  0 - 20 <br />\
     <img src="styles/legend/Calidaddeaguaensucantnsehamantenidoigual_4_1.png" />  20 - 40 <br />\
     <img src="styles/legend/Calidaddeaguaensucantnsehamantenidoigual_4_2.png" />  40 - 60 <br />\
     <img src="styles/legend/Calidaddeaguaensucantnsehamantenidoigual_4_3.png" />  60 - 80 <br />\
-    <img src="styles/legend/Calidaddeaguaensucantnsehamantenidoigual_4_4.png" />  80 - 100 <br />'
+    <img src="styles/legend/Calidaddeaguaensucantnsehamantenidoigual_4_4.png" />  80 - 100 <br /><br />'
         });
 
-lyr_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0.setVisible(true);lyr_Elconsumodeaguaseaunserviciopblico_1.setVisible(true);lyr_NotieneconocimientosobrelaLeydeAguas_2.setVisible(true);lyr_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3.setVisible(true);lyr_Calidaddeaguaensucantnsehamantenidoigual_4.setVisible(true);
+lyr_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0.setVisible(false);lyr_Elconsumodeaguaseaunserviciopblico_1.setVisible(false);lyr_NotieneconocimientosobrelaLeydeAguas_2.setVisible(true);lyr_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3.setVisible(false);lyr_Calidaddeaguaensucantnsehamantenidoigual_4.setVisible(false);
 var layersList = [baseLayer,lyr_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0,lyr_Elconsumodeaguaseaunserviciopblico_1,lyr_NotieneconocimientosobrelaLeydeAguas_2,lyr_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3,lyr_Calidaddeaguaensucantnsehamantenidoigual_4];
 lyr_Pagarelusodeaguadependiendodelacantidadqueutilicecadafamiliaperoconexcepciones_0.set('fieldAliases', {'Codigo': 'Codigo', 'Cantón': 'Cantón', 'Provincia': 'Provincia', 'Grafica': 'Grafica', 'Si, pero c': 'Si, pero c', });
 lyr_Elconsumodeaguaseaunserviciopblico_1.set('fieldAliases', {'Codigo': 'Codigo', 'Cantón': 'Cantón', 'Provincia': 'Provincia', 'Grafica': 'Grafica', 'A14_Públic': 'A14_Públic', });
@@ -113,4 +113,5 @@ lyr_Reducirlosproblemasdeaguasiendoeficientesconsuusoencasa_3.set('fieldLabels',
 lyr_Calidaddeaguaensucantnsehamantenidoigual_4.set('fieldLabels', {'Cantón': 'no label', 'Provincia': 'no label', 'Grafica': 'no label', 'A10_Se man': 'no label', });
 lyr_Calidaddeaguaensucantnsehamantenidoigual_4.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
+	
 });
